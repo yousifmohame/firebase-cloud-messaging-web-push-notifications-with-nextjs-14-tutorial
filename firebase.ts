450f57360f0e60f955e33d4f9d6fcab13b30275a
kapsa <75359352+yousifmohame@users.noreply.github.com>
2025-02-06 16:12:38 +0200

    Add files via upload

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,14 @@ import { getMessaging, getToken, isSupported } from "firebase/messaging";
 
 // Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: null,
-  authDomain: null,
-  projectId: null,
-  storageBucket: null,
-  messagingSenderId: null,
-  appId: null,
-  measurementId: null,
+  apiKey: "AIzaSyAA1NB3xLMUvAGg1ni4m1JK_qrI96CSnGI",
+  authDomain: "filmy-6bd1a.firebaseapp.com",
+  databaseURL: "https://filmy-6bd1a-default-rtdb.firebaseio.com",
+  projectId: "filmy-6bd1a",
+  storageBucket: "filmy-6bd1a.appspot.com",
+  messagingSenderId: "409354230199",
+  appId: "1:409354230199:web:978e2be5c80f52a197b032",
+  measurementId: "G-3CGY3MPEKT"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
